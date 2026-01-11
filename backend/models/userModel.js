afd2831cfb1fc917210dd4271a6debff039daa85
SubodhCraft <nepaliboiz123@gmail.com>
2026-01-11 12:13:35 +0545

    Added

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,40 +1,49 @@
-const {DataTypes}=require("sequelize");
-const { sequelize } = require ("../database/Database")
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../database/db");
 
 const User = sequelize.define(
-    "User",
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement : true,
-            primaryKey: true,
-        },
-        username:{
-            type: DataTypes.STRING,
-            allowNull:false,
-            unique:true,
-        },
-        email:{
-            type: DataTypes.STRING,
-            allowNull:false,
-            unique:true,
-            validate: {
-                isEmail: true,
-            },
-        },
-        password:{
-            type: DataTypes.STRING,
-            allowNull:false,
-        },
-        role:{
-            type: DataTypes.ENUM('user','admin'),
-            defaultValue:'user',
-        },
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    {
-        tableName:"users",
-        timestamps:true,
-    }
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.ENUM("user", "admin"),
+      defaultValue: "user",
+    },
+    // Password reset fields
+    resetPasswordToken: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    resetPasswordExpires: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  },
+  {
+    tableName: "users",
+    timestamps: true,
+  }
 );
 
-module.exports=User;
+module.exports = User;
