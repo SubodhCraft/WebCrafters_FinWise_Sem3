70398ae36aa947fca50227556b2958806b9683b6
SubodhCraft <nepaliboiz123@gmail.com>
2026-01-11 12:14:31 +0545

    routes file added

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,13 +4,14 @@ const router = express.Router();
 const { register } = require("../controllers/registerController");
 const { login } = require("../controllers/loginController");
 const { getProfile, updateProfile, getAllUsers } = require("../controllers/userController");
+const authGuard = require("../helpers/authGuard");
 
 router.post("/register", register);
 router.post("/login", login);
 
-router.get("/getAllUsers", getAllUsers);
+router.get("/getAllUsers", authGuard, getAllUsers);
 router.get("/getUserById/:id", getProfile);
 router.put("/updateUserById/:id", updateProfile);
 
-router.get('get_all_users', getAllUsers);
+router.get('/get_all_users', getAllUsers);
 module.exports = router;
